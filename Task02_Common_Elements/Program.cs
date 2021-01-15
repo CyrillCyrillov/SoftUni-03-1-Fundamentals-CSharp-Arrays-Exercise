43140767364be56d29b4dba712cdd9ea62302228
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-15 22:10:23 +0200

    Add files via upload

---
+++ Task02_Common_Elements/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Task02_Common_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr1 = Console.ReadLine().Split().ToArray();
+            string[] arr2 = Console.ReadLine().Split().ToArray();
+
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                for (int j = arr1.Length - 1; j >= 0; j--)
+                {
+                    if(arr1[j] == arr2[i])
+                    {
+                        Console.Write($"{arr1[j]} ");
+                    }
+                }
+            }
+        }
+    }
+}
