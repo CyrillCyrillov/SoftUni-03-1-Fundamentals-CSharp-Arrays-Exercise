81dca41c62cc1850982554b7e294bec55a0e308e
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-15 22:16:45 +0200

    Add files via upload

---
+++ Task05_Top_Integers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Task05_Top_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                bool integerNumber = true;
+                for (int j = i+1; j < numbers.Length; j++)
+                {
+                    if(numbers[i] <= numbers[j])
+                    {
+                        integerNumber = false;
+                        break;
+                    }
+                }
+                
+                if(integerNumber)
+                {
+                    Console.Write($"{numbers[i]} ");
+                }
+            }
+        }
+    }
+}
