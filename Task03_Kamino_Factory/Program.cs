2705e781c2127d24cd385b6a06fb2a0206df3c77
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-15 22:11:49 +0200

    Add files via upload

---
+++ Task03_Kamino_Factory/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task03_Kamino_Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dnaSize = int.Parse(Console.ReadLine());
+
+            string comand = string.Empty;
+
+            while ((comand = Console.ReadLine()) != "Clone Them!")
+            {
+
+            }
+            
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
