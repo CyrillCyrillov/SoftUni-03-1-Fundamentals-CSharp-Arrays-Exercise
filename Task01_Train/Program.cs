a7e9740e1e12fe23fa99e7853061051a26615801
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-15 22:09:27 +0200

    Add files via upload

---
+++ Task01_Train/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _Task01_Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbersOfWagons = int.Parse(Console.ReadLine());
+            int[] wagon = new int[numbersOfWagons];
+
+            for (int i = 0; i <= numbersOfWagons - 1; i++)
+            {
+                wagon[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i <= numbersOfWagons - 1; i++)
+            {
+                Console.Write($"{wagon[i]} ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(wagon.Sum());
+        }
+    }
+}
